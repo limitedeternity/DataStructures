af22a8077b1fc5c74f2cd6a25e2d03fac5bd3306
Marise Hayashi <vyatheslav2707@mail.ru>
2021-04-19 00:19:48 +0300

    Stack implemented

--- 4Stacks/Stack.hpp
+++ 4Stacks/Stack.hpp
@@ -1,10 +1,11 @@
+#pragma once
+
 #include "../3LinkedLists/SinglyLinkedList.hpp"
 
 template <typename T>
-class Stack : private SinglyLinkedList<T> {
+class Stack : public SinglyLinkedList<T> {
 public:
     Stack() : SinglyLinkedList<T>() {}
-    // copy/move constructors
 
     Maybe<T> pop() noexcept {
         auto result = this->get();
@@ -19,16 +20,9 @@ public:
     bool contains(T const& elem) const noexcept {
         return this->indexOf(elem).isJust();        
     }
-
-    using SinglyLinkedList<T>::clear;
-    using SinglyLinkedList<T>::sizeOf;
-
-    friend std::ostream& operator<<(std::ostream& os, Stack const& s) {
-        return os << s.toString();
-    }
 };
 
-int main(void) {
+/*int main(void) {
     Stack<int> s;
     s.push(5);
 
@@ -37,4 +31,4 @@ int main(void) {
     std::cout << "Stack top: " << s.pop() << std::endl;
     std::cout << "Stack size after pop: " << s.sizeOf() << std::endl;
     std::cout << "Exhausted stack top: " << s.pop() << std::endl;
-}
+}*/
