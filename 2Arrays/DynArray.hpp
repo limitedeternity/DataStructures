3f1d43b23e2e6196852aa1adb04082264228db18
Vyacheslav Bespalov <vyatheslav2707@mail.ru>
2021-05-08 20:43:00 +0300

    DynArray: optimizations

--- 2Arrays/DynArray.hpp
+++ 2Arrays/DynArray.hpp
@@ -10,136 +10,158 @@
 template <typename T>
 class DynArray {
 public:
-    DynArray(size_t size = 10) noexcept : 
-        pa(non_std::make_unique<T[]>(size)),
-        length{size},
-        nextIndex{0}
-    {}
-
-    virtual ~DynArray() = default;
-
-    DynArray(const DynArray& source) noexcept :
-        pa(non_std::make_unique<T[]>(source.length)),
-        length{source.length},
-        nextIndex{source.nextIndex}
-    {
-        std::memcpy(pa.get(), source.pa.get(), sizeof(T) * source.length);
+    DynArray(size_t size = 10) {
+        internal_reserve(!size ? 1 : size);
     }
 
-    DynArray& operator=(const DynArray& source) noexcept {
+    virtual ~DynArray() noexcept {
+        free(m_buffer);
+    }
+
+    DynArray(const DynArray& source) {
+        if (!source.size()) {
+            internal_reserve(1);
+        }
+        else {
+            append(source.data(), source.size());
+        }
+    }
+
+    DynArray& operator=(const DynArray& source) {
         if (std::addressof(*this) != std::addressof(source)) {
-            pa = non_std::make_unique<T[]>(source.length);
-            length = source.length;
-            nextIndex = source.nextIndex;
-            std::memcpy(pa.get(), source.pa.get(), sizeof(T) * source.length);
+            clear();
+            append(source.data(), source.size());
         }
 
         return *this;
     }
 
-    DynArray(DynArray&& source) noexcept :
-        length{source.length},
-        nextIndex{source.nextIndex}
-    {
-        pa.reset(source.pa.release());
+    DynArray(DynArray&& source) noexcept {
+        if (!source.size()) {
+            internal_reserve(1);
+        }
+        else {
+            append(source.data(), source.size(), true);
+        }
     }
 
     DynArray& operator=(DynArray&& source) noexcept {
-        length = source.length;
-        nextIndex = source.nextIndex;
-        pa.reset(source.pa.release());
+        clear();
+        append(source.data(), source.size(), true);
         return *this;
     }
 
+    void reserve(size_t reserve_size) {
+        if (reserve_size > m_capacity) {
+            internal_reserve(reserve_size);
+        }
+    }
+
+    void resize(size_t new_len) {
+        if (new_len > m_capacity) {
+            internal_reserve(new_len);
+        }
+
+        m_size = new_len;
+    }
+
+    T* data() const noexcept {
+        return m_buffer;
+    }
+
     T& operator[](size_t index) {
-        if (nextIndex == 0 || index >= nextIndex) {
+        if (index >= m_size) {
             throw std::out_of_range("Out of range");
         }
 
-        return pa[index];
+        return m_buffer[index];
     }
 
     Maybe<T> at(size_t index) const noexcept {
-        if (nextIndex == 0) {
+        if (index >= m_size) {
             return Maybe<T>();
         }
 
-        return return_<Maybe>(pa[index >= nextIndex ? nextIndex - 1 : index]);
+        return return_<Maybe>(m_buffer[index]);
     }
 
-    void add(T const& val) noexcept {
-        if (nextIndex == length) {
-            if (length == 0) length = 1;
-            else length *= 2;
-
-            auto prev = pa.release();
-            pa = non_std::make_unique<T[]>(length);
-            std::memcpy(pa.get(), prev, sizeof(T) * nextIndex);
-
-            delete[] prev;
-        }
-
-        pa[nextIndex++] = val;
+    void add(T const& val) {
+        resize(m_size + 1);
+        m_buffer[m_size - 1] = val;
     }
 
-    void insertAt(size_t index, T const& val) noexcept {
-        if (index >= nextIndex) return add(val);
+    void append(const T* data, size_t len, bool move_allowed = false) {
+        if (!len) return;
 
-        auto prev = pa.release();
-        pa = non_std::make_unique<T[]>(length + 1);
+        uint64_t this_range_start = (uintptr_t)m_buffer;
+        uint64_t this_range_end = (uintptr_t)(m_buffer + m_size);
+        uint64_t buffer_range_start = (uintptr_t)data;
+        uint64_t buffer_range_end = (uintptr_t)(data + len);
 
-        if (index > 0) std::memcpy(pa.get(), prev, sizeof(T) * index);
-        pa[index] = val;
-        std::memcpy(pa.get() + index + 1, prev + index, sizeof(T) * (nextIndex - index));
+        if (buffer_range_end == this_range_end &&
+            buffer_range_start >= this_range_start) {
 
-        delete[] prev;
-        length++;
-        nextIndex++;
-    }
-
-    void removeAt(size_t index) noexcept {
-        if (nextIndex == 0) return;
-        if (index >= nextIndex - 1) {
-            pa[nextIndex - 1].~T();
-            nextIndex--;
             return;
         }
 
-        auto prev = pa.release();
-        pa = non_std::make_unique<T[]>(length - 1);
+        size_t pos = m_size;
+        size_t new_pos = pos + len;
+        resize(new_pos);
 
-        if (index > 0) std::memcpy(pa.get(), prev, sizeof(T) * index);
-        if (nextIndex - index - 1 > 0) 
-            std::memcpy(pa.get() + index, prev + index + 1, sizeof(T) * (nextIndex - index - 1));
+        if (move_allowed) {
+            memmove(m_buffer + pos, data, len);
+        }
+        else {
+            memcpy(m_buffer + pos, data, len);
+        }
+    }
 
-        delete[] prev;
-        length--;
-        nextIndex--;
+    void insertAt(size_t index, T const& val) noexcept {
+        if (index >= m_size) return add(val);
+
+        resize(m_size + 1);
+        memmove(m_buffer + index + 1, m_buffer + index, sizeof(T) * (m_size - index - 1));
+        m_buffer[index] = val;
+    }
+
+    void removeAt(size_t index) noexcept {
+        if (!m_size) return;
+        if (index >= m_size - 1) {
+            m_buffer[m_size - 1].~T();
+            m_size--;
+            return;
+        }
+
+        m_buffer[index].~T();
+        memmove(m_buffer + index, m_buffer + index + 1, sizeof(T) * (m_size - index - 1));
+        m_size--;
     }
 
     void clear() noexcept {
-        pa = non_std::make_unique<T[]>(10);
-        length = 10;
-        nextIndex = 0;
+        for (size_t i = 0; i < m_size; ++i) {
+            m_buffer[i].~T();
+        }
+
+        m_size = 0;
     }
 
     size_t size() const noexcept {
-        return nextIndex;
+        return m_size;
     }
 
     friend std::string to_string(DynArray const& d) noexcept {
         std::stringstream ss;
         ss << "[";
-        if (d.nextIndex == 0) {
+        if (!d.m_size) {
             ss << "]";
             return ss.str();
         }
 
-        for (size_t i = 0; i < d.nextIndex - 1; ++i) {
+        for (size_t i = 0; i < d.m_size - 1; ++i) {
             ss << non_std::to_string(d.at(i).fromJust()) << ", ";
         }
 
-        ss << non_std::to_string(d.at(d.nextIndex - 1).fromJust()) << "]";
+        ss << non_std::to_string(d.at(d.m_size - 1).fromJust()) << "]";
         return ss.str();
     }
 
@@ -156,11 +178,11 @@ public:
             return true;
         }
 
-        if (nextIndex != rhs.nextIndex) {
+        if (m_size != rhs.m_size) {
             return false;
         }
 
-        for (size_t i = 0; i < nextIndex; ++i) {
+        for (size_t i = 0; i < m_size; ++i) {
             if (at(i) != rhs.at(i)) {
                 return false;
             }
@@ -170,9 +192,19 @@ public:
     }
 
 private:
-    std::unique_ptr<T[]> pa;
-    size_t length;
-    size_t nextIndex;
+    void internal_reserve(size_t reserve_size) {
+        T* new_buffer = (T*)realloc(m_buffer, reserve_size * sizeof(T));
+        if (new_buffer == nullptr) {
+            throw std::bad_alloc();
+        }
+
+        m_capacity = reserve_size;
+        m_buffer = new_buffer;
+    }
+
+    size_t m_size = 0;
+    size_t m_capacity = 0;
+    T* m_buffer = nullptr;
 };
 
 /*int main(void) {
@@ -186,7 +218,7 @@ private:
     std::cout << d << std::endl;
 
     d.removeAt(4);
-    d.removeAt(1);
+    d.removeAt(0);
     d.add(50);
 
     std::cout << d << std::endl;
